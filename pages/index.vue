575e206099f8fe3bf2949470a90b5233329a7b12
ec50n9 <shelloworld@qq.com>
2024-01-06 14:41:22 +0800

    增加实验室路由

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,21 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+useHead({
+  title: "ec50n9 的杂货铺",
+});
+
+const navList = useState("navList", () => [
+  { name: "实验室", path: "/lab" },
+  { name: "关于", path: "/about" },
+]);
+</script>
 
 <template>
   <div class="p-4">
-    <h1 class="my-4 text-3xl c-blue">ec50n9 的杂货铺</h1>
-    <NuxtLink to="/about">关于</NuxtLink>
+    <h1 class="my-4 text-3xl c-blue-5">ec50n9 的杂货铺</h1>
+    <ul class="flex items-center flex-wrap gap-3">
+      <li v-for="item in navList" :key="item.path" class="px-4 py-2 rd-full bg-blue-50 c-blue-5">
+        <NuxtLink :to="item.path">{{ item.name }}</NuxtLink>
+      </li>
+    </ul>
   </div>
 </template>
---
+++ pages/lab/index.vue
@@ -0,0 +1,5 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div>这里是实验室</div>
+</template>

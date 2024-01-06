8f6a55aada00d11be1897dc80e9e08ee203daa0f
ec50n9 <shelloworld@qq.com>
2024-01-06 16:58:36 +0800

    修改实验室标题

--- pages/lab/index.vue
+++ pages/lab/index.vue
@@ -1,12 +1,12 @@
 <script lang="ts" setup>
 useHead({
-  title: "小蜗实验室",
+  title: "神奇小蜗实验室",
 });
 </script>
 
 <template>
   <div>
-    <AppHeader title="小蜗实验室" />
+    <AppHeader title="神奇小蜗实验室" />
     <ul class="p-4">
       <li class="c-emerald-7 underline">
         <NuxtLink to="/lab/magic-text-bg">神奇文字背景</NuxtLink>

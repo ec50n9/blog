cd9b77a9ff07f64516b5f3f4adb86152dd880cc7
ec50n9 <shelloworld@qq.com>
2024-01-14 14:37:59 +0800

    pref: 把组件的演示页面拆分到子页面

--- pages/ui-lab/index.vue
+++ pages/ui-lab/index.vue
@@ -1,20 +1,25 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const navList = useState(() => [
+  { title: "开关", path: "/ui-lab/toggle", name: "ui-toggle-title" },
+  { title: "键盘文本", path: "/ui-lab/kbd", name: "ui-kbd-title" },
+]);
+</script>
 
 <template>
-  <div class="of-auto">
+  <div class="min-h-screen of-auto">
     <!-- 标题 -->
     <h1 m="t-6 l-5" text="3xl">
       <span view-transition-name="ui-lab">UI 实验室</span>
     </h1>
 
     <!-- 开关提示 -->
-    <div flex items-center m="5" p="x-5 y-3" bg="slate-1" rd-xl>
+    <div flex="~ items-center gap-3" m="5" p="x-5 y-3" bg="slate-1" rd-xl>
       <!-- 左边文本 -->
       <div grow>
         <div flex items-center text="md">
-          <span inline-block mr-1>🔔</span>打开通知权限
+          <span inline-block mr-1>🔔</span>一个开关
         </div>
-        <div text="sm slate-4">打开通知权限可以收到通知！</div>
+        <div text="sm slate-4">没什么用，但是会浪费你几秒钟来阅读这句话。</div>
       </div>
       <!-- 右边开关 -->
       <div shrink-0>
@@ -25,35 +30,18 @@
       </div>
     </div>
 
-    <!-- 带文本的开关 -->
-    <div flex flex-wrap items-center gap-3 m-5>
-      <label toggle-wrapper>
-        <input type="checkbox" value="" toggle-input class="peer" />
-        <div toggle-bg-emerald></div>
-        <span ms-3 text="sm gray-9 dark:gray-3" font="medium">点击切换</span>
-      </label>
-
-      <label class="toggle-wrapper">
-        <input type="checkbox" value="" class="toggle-input peer" disabled />
-        <div class="toggle-bg-blue"></div>
-        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >禁用</span
+    <ul m="x-5" flex="~ wrap gap-5">
+      <li
+        v-for="item in navList"
+        :key="item.path"
+        class="c-emerald-6 underline"
+      >
+        <NuxtLink :to="item.path"
+          ><span :view-transition-name="item.name">{{
+            item.title
+          }}</span></NuxtLink
         >
-      </label>
-
-      <label class="toggle-wrapper">
-        <input
-          type="checkbox"
-          value=""
-          class="toggle-input peer"
-          checked
-          disabled
-        />
-        <div class="toggle-bg-red"></div>
-        <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300"
-          >禁用</span
-        >
-      </label>
-    </div>
+      </li>
+    </ul>
   </div>
 </template>
---
+++ pages/ui-lab/toggle.vue
@@ -0,0 +1,73 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="of-auto">
+    <!-- 标题 -->
+    <h1 m="t-6 l-5" text="3xl">
+      <span view-transition-name="ui-toggle-title">开关</span>
+    </h1>
+
+    <!-- 带文本 -->
+    <div flex="~ wrap" items-center gap-3 m-5>
+      <label toggle-wrapper>
+        <input type="checkbox" value="" toggle-input class="peer" />
+        <div toggle-bg-emerald></div>
+        <span ms-3 text="sm gray-9 dark:gray-3" font="medium"
+          >带文本的开关</span
+        >
+      </label>
+    </div>
+
+    <!-- 禁用 -->
+    <div flex flex-wrap items-center gap-3 m-5>
+      <label toggle-wrapper>
+        <input type="checkbox" value="" class="toggle-input peer" disabled />
+        <div toggle-bg-blue></div>
+        <span ms-3 text="sm gray-9 dark:gray-3" font="medium"
+          >禁用</span
+        >
+      </label>
+
+      <label class="toggle-wrapper">
+        <input
+          type="checkbox"
+          value=""
+          class="toggle-input peer"
+          checked
+          disabled
+        />
+        <div toggle-bg-red></div>
+        <span ms-3 text="sm gray-9 dark:gray-3" font="medium"
+          >禁用</span
+        >
+      </label>
+    </div>
+
+    <!-- 尺寸 -->
+    <div flex flex-wrap items-center gap-3 m-5>
+      <label toggle-wrapper>
+        <input type="checkbox" value="" toggle-input class="peer" checked />
+        <div toggle="bg-indigo size-sm"></div>
+        <span ms-3 text="sm gray-9 dark:gray-3" font="medium"
+          >小号</span
+        >
+      </label>
+
+      <label toggle-wrapper>
+        <input type="checkbox" value="" toggle-input class="peer" checked />
+        <div toggle="bg-indigo size-md"></div>
+        <span ms-3 text="sm gray-9 dark:gray-3" font="medium"
+          >中号</span
+        >
+      </label>
+
+      <label toggle-wrapper>
+        <input type="checkbox" value="" toggle-input class="peer" checked />
+        <div toggle="bg-indigo size-lg"></div>
+        <span ms-3 text="sm gray-9 dark:gray-3" font="medium"
+          >大号</span
+        >
+      </label>
+    </div>
+  </div>
+</template>

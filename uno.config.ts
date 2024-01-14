ab5db4e72504e05ecc785318fb6d07d28016dc4e
ec50n9 <shelloworld@qq.com>
2024-01-14 13:26:51 +0800

    pref: 开关增加尺寸选项

--- uno.config.ts
+++ uno.config.ts
@@ -21,12 +21,22 @@ export default defineConfig({
       /^toggle-bg(-(.+))?$/,
       ([, , c = "blue"]) =>
         [
-          "w-11 h-6 bg-gray-200 rounded-full dark:bg-gray-700 dark:border-gray-600",
+          "bg-gray-200 rounded-full dark:bg-gray-700 dark:border-gray-600",
           "transition",
-          "after:absolute after:top-[2px] after:start-[2px] after:h-5 after:w-5 after:content-[''] after:bg-white after:border-gray-300 after:border after:rounded-full after:transition-all",
+          "after:absolute after:content-[''] after:bg-white after:border-gray-300 after:border after:rounded-full after:transition-all",
           `peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-${c}-300 dark:peer-focus:ring-${c}-800`,
           `peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white peer-checked:bg-${c}-600`,
+          "w-11 h-6 after:top-[2px] after:start-[2px] after:h-5 after:w-5",
         ].join(" "),
+    ],
+    [
+      /^toggle-size-(.+)$/,
+      ([, s = "md"]) =>
+        ({
+          sm: "w-9 h-5 after:top-[2px] after:start-[2px] after:h-4 after:w-4",
+          md: "w-11 h-6 after:top-[2px] after:start-[2px] after:h-5 after:w-5",
+          lg: "w-14 h-7 after:top-0.5 after:start-[4px] after:h-6 after:w-6",
+        }[s]),
     ],
   ],
 });

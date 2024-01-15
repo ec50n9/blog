620ee1ba88650e0254e33154584b12ba4cc3ce0d
ec50n9 <shelloworld@qq.com>
2024-01-16 01:16:20 +0800

    feat: 用户jwt信息增加is_admin字段

--- laf/functions/common_types.ts
+++ laf/functions/common_types.ts
@@ -1,7 +1,11 @@
 export interface BaseDO {
-  _id?: string
   created_by: string;
   created_time: number;
   updated_by?: string;
   updated_time?: number;
+}
+
+export interface JwtUserInfo {
+  uid: string;
+  is_admin: boolean;
 }
